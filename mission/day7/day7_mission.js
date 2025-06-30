a4492392df5ad88d31e69eb5f1e6fa1e37fb5e29
김요한 <kimyohan@gim-yohan-ui-MacBookPro.local>
2025-06-30 22:04:40 +0900

    [refactor] 미션 코드 주석 추가

--- mission/day7/day7_mission.js
+++ mission/day7/day7_mission.js
@@ -12,6 +12,7 @@ console.log($text);
 $button.id = "new-button"; // id 추가
 // 버튼 추가 이벤트 리스너와 textContent를 통해 변경한다.
 $button.addEventListener("click", () => {
+  // textContent: 요소의 텍스트 내용을 바꿉니다. (HTML 태그는 적용되지 않음)
   $text.textContent = "오늘도 고생했어!";
   console.log("버튼 클릭됨");
   console.log($text.textContent);
