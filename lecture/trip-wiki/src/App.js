7349aba58fbaa7bd194e6871417de1bd6fa0e68a
김요한 <rladygks1210@naver.com>
2025-07-18 03:11:00 +0900

    [feat] Header 컴포넌트 추가

--- lecture/trip-wiki/src/App.js
+++ lecture/trip-wiki/src/App.js
@@ -6,15 +6,56 @@ import CityDetail from "./components/CityDetail.js";
 import { request } from "./components/api.js";
 
 export default function App ($app) {
+    const getSortBy = () => {
+        if (window.location.search) {
+            return window.location.search.split('sort=')[1].split('&')[0];
+        }
+        return 'total';
+    };
+
+    const getSearchWord = () => {
+        if (window.location.search && window.location.search.includes('search=')) {
+            return window.location.search.split('search=')[1];
+        }
+        return '';
+    };
+
     this.state = {
         startIdx: 0,
-        sortBy: "",
-        searchWord: "",
+        sortBy: getSortBy(),
+        searchWord: getSearchWord(),
         region: "",
         cities: "",
     };
 
-    const header = new Header();
+    const header = new Header({$app, initialState: {sortBy:this.sortBy, searchWord: this.state.searchWord}, 
+        handleSortChange: async(sortBy) => {
+            const pageUrl = `/${this.state.region}?sort=${sortBy}`;
+            history.pushState(
+                null, 
+                null, 
+                this.state.searchWord ? pageUrl + `&search=${this.state.searchWord}` : pageUrl
+            );
+            const cities = await request(0, this.state.region, sortBy, this.state.searchWord);
+            this.setState({
+                ...this.state,
+                startIdx: 0,
+                sortBy: sortBy,
+                cities: cities, 
+            });
+        },
+        handleSearch: async (searchWord) => {
+            history.pushState(null, null, `/${this.state.region}?sort=${this.state.sortBy}&search=${searchWord}`);
+            const cities = await request(0, this.state.region, this.state.sortBy, searchWord);
+            this.setState({
+                ...this.state,
+                startIdx: 0,
+                searchWord: searchWord,
+                cities: cities,
+            })
+        },
+    });
+
     const regionList = new RegionList();
     const cityList = new CityList({$app, initialState: this.state.cities, 
         handleLoadMore: async () => {
@@ -35,7 +76,8 @@ export default function App ($app) {
     this.setState = (newState) => {
         this.state = newState;
         cityList.setState(this.state.cities);
-    }
+        header.setState({sortBy: this.state.sortBy, searchWord: this.state.searchWord});
+    };
 
     const init = async () => {
         const cities = await request(this.state.startIdx, this.state.region, this.state.sortBy, this.state.searchWord);
