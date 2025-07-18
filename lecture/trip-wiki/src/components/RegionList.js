d3f3d7fa05c876bfcd98079eef2ec3aba2c418d4
김요한 <rladygks1210@naver.com>
2025-07-18 21:42:40 +0900

    [feat] header 컴포넌트 추가 및 missionDay13 추가

--- lecture/trip-wiki/src/components/RegionList.js
+++ lecture/trip-wiki/src/components/RegionList.js
@@ -1,14 +1,52 @@
-export default function RegionList () {
-    this.$target = document.createElement('div');
-    this.$target.className = 'region-list';
+export default function RegionList({ $app, initialState, handleRegion }) {
+  this.state = initialState;
 
-    this.template = () => {};
+  this.$target = document.createElement("div");
+  this.$target.className = "region-list";
 
-    this.render = () => {};
+  this.handleRegion = handleRegion;
+  $app.appendChild(this.$target);
 
-    this.setState = (newState) => {
-        this.state = newState;
-        this.render();
-    };
+  this.template = () => {
+    const regionList = [
+      "🚀 All",
+      "🌏 Asia",
+      "🕌 Middle-East",
+      "🇪🇺 Europe",
+      "💃 Latin-America",
+      "🐘 Africa",
+      "🏈 North-America",
+      "🏄 Oceania",
+    ];
+    let temp = ``;
+    regionList.forEach((el) => {
+      let reginId = el.split(" ")[1];
+      temp += `<div id=${reginId}>${el}</div>`;
+    });
+
+    return temp;
+  };
+
+  this.render = () => {
+    this.$target.innerHTML = this.template();
+    if (this.state) {
+      let $currentRegion = document.getElementById(this.state);
+      $currentRegion && ($currentRegion.className = "clicked");
+    } else {
+      document.getElementById("All").className = "clicked";
+    }
+
+    const $regionList = this.$target.querySelectorAll("div");
+    $regionList.forEach((el) => {
+      el.addEventListener("click", () => {
+        this.handleRegion(el.id);
+      });
+    });
+  };
+
+  this.setState = (newState) => {
+    this.state = newState;
     this.render();
+  };
+  this.render();
 }
